f159a1055242b22107e985aa230fbb1ad8ff2a95
oezdemir <ozdemir@cip.ifi.lmu.de>
2025-09-10 19:07:34 +0200

    realistic bounce logic

--- particle.py
+++ particle.py
@@ -20,17 +20,25 @@ class Particle:
         self.x = random.randint(50, WIDTH-50)
         self.y = random.randint(50, HEIGHT-50)
         self.vx, self.vy = random_velocity()
-        self.radius = 30
-        self.mass = float(self.radius)
+        self.radius =10
+        self.mass = 10.0
         self.color = (random.randint(100,255), random.randint(100,255), random.randint(100,255))
     def move(self):
         self.x += self.vx
         self.y += self.vy
-        # Bounce off walls
-        if self.x < self.radius or self.x > WIDTH - self.radius:
-            self.vx *= -1
-        if self.y < self.radius or self.y > HEIGHT - self.radius:
-            self.vy *= -1
+        # Realistic wall bounce
+        if self.x < self.radius:
+            self.x = self.radius
+            self.vx = -self.vx
+        elif self.x > WIDTH - self.radius:
+            self.x = WIDTH - self.radius
+            self.vx = -self.vx
+        if self.y < self.radius:
+            self.y = self.radius
+            self.vy = -self.vy
+        elif self.y > HEIGHT - self.radius:
+            self.y = HEIGHT - self.radius
+            self.vy = -self.vy
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.radius)
 
@@ -67,34 +75,53 @@ def apply_elastic_impulse(a, b, restitution=1.0):
         dist = 1e-6
     nx = dx / dist
     ny = dy / dist
-    rvx = a.vx - b.vx
-    rvy = a.vy - b.vy
-    vel_along_normal = rvx * nx + rvy * ny
-    if vel_along_normal > 0:
-        return
-    invA = 1.0 / a.mass
-    invB = 1.0 / b.mass
-    j = -(1.0 + restitution) * vel_along_normal
-    j /= (invA + invB)
-    impulse_x = j * nx
-    impulse_y = j * ny
-    a.vx += impulse_x * invA
-    a.vy += impulse_y * invA
-    b.vx -= impulse_x * invB
-    b.vy -= impulse_y * invB
+    # Tangent vector
+    tx = -ny
+    ty = nx
+    # Velocities along normal and tangent
+    va_n = a.vx * nx + a.vy * ny
+    vb_n = b.vx * nx + b.vy * ny
+    va_t = a.vx * tx + a.vy * ty
+    vb_t = b.vx * tx + b.vy * ty
+    # Only swap normal components
+    m1, m2 = a.mass, b.mass
+    va_n_new = (va_n * (m1 - m2) + 2 * m2 * vb_n) / (m1 + m2)
+    vb_n_new = (vb_n * (m2 - m1) + 2 * m1 * va_n) / (m1 + m2)
+    # Update velocities
+    a.vx = va_n_new * nx + va_t * tx
+    a.vy = va_n_new * ny + va_t * ty
+    b.vx = vb_n_new * nx + vb_t * tx
+    b.vy = vb_n_new * ny + vb_t * ty
 
-def handle_collisions(particles):
-    n = len(particles)
-    for i in range(n):
-        for j in range(i + 1, n):
-            a = particles[i]
-            b = particles[j]
-            if circles_collide(a, b):
-                resolve_overlap(a, b)
-                apply_elastic_impulse(a, b, restitution=1.0)
+def handle_collisions_grid(particles, cell_size=40):
+    grid = {}
+    for idx, p in enumerate(particles):
+        cell_x = int(p.x // cell_size)
+        cell_y = int(p.y // cell_size)
+        key = (cell_x, cell_y)
+        if key not in grid:
+            grid[key] = []
+        grid[key].append(idx)
+    checked = set()
+    for key, indices in grid.items():
+        neighbors = [key,
+                     (key[0]+1, key[1]), (key[0]-1, key[1]),
+                     (key[0], key[1]+1), (key[0], key[1]-1),
+                     (key[0]+1, key[1]+1), (key[0]-1, key[1]-1),
+                     (key[0]+1, key[1]-1), (key[0]-1, key[1]+1)]
+        for nkey in neighbors:
+            if nkey in grid:
+                for i in indices:
+                    for j in grid[nkey]:
+                        if i < j and (i, j) not in checked:
+                            a, b = particles[i], particles[j]
+                            if circles_collide(a, b):
+                                resolve_overlap(a, b)
+                                apply_elastic_impulse(a, b, restitution=1.0)
+                            checked.add((i, j))
 
 # Create 10 particles
-particles = [Particle() for _ in range(10)]
+particles = [Particle() for _ in range(20)]
 
 
     
@@ -108,7 +135,7 @@ while running:
     screen.fill((30, 30, 30))  # Frame background
     for p in particles:
         p.move()
-    handle_collisions(particles)
+    handle_collisions_grid(particles, cell_size=40)
     for p in particles:
         p.draw(screen)
     pygame.display.flip()
